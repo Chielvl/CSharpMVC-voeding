eec5d52a0890b07b41ebd907054aba5fcaa41734
Chiel <chielvanleeuwen@hotmail.com>
2022-01-14 11:24:22 +0100

    eerste commit

---
+++ CSharpMVC-voeding/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSharpMVC_voeding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
